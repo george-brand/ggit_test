232ccd0ed1dcb44bcb1d222bcf9c92f3fca80d1c
George <george.n.brand@icloud.com>
2021-11-20 16:03:00 -0500

    Added print statement to test_1.py.

--- test_1.py
+++ test_1.py
@@ -4,3 +4,7 @@ print("Experimenting with dev branch and pull requests.")
 
 print("Experimenting with feature branches.")
 print("Making multiple commits.")
+
+print("New branch dev_update_2 copied from dev_update.  Going to push updates from this
+    new branch up to remote repository, do a pull request to merge with main branch and then
+    do a pull to update main branch on local repository.")
