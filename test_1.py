796ac401a6969e1a34d5110561fd3b251fd4a394
George <george.n.brand@icloud.com>
2021-11-21 19:43:04 -0500

    Added new comment to test_1.py.

--- test_1.py
+++ test_1.py
@@ -1,3 +1,5 @@
 # This is a junk Python script that I will be making changes to for testing Git features.
 
 print("Experimenting with dev branch and pull requests.")
+
+print("Adding a new comment from dev branch.")
